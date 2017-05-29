1175411e72aae72f49a0286b45e7b4a634d87b71
Peter Rice <ricepv@dukes.jmu.edu>
2017-05-29 10:47:51 -0400

    fish is fine by default

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,2 @@
-if status --is-interactive
-    keychain --eval --quiet --noask id_rsa | source
-end
-
-fish_vi_key_bindings
+alias startscribble='systemctl --user start mpdscribble'
+alias stopscribble='systemctl --user stop mpdscribble'
