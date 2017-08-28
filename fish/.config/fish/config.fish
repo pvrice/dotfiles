8f5d9df4c63f2f6948a6234c60d064f98e90472f
Peter Rice <ricepv@dukes.jmu.edu>
2017-08-27 23:01:14 -0400

    use keychain globally

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,11 @@
 alias startscribble='systemctl --user start mpdscribble'
 alias stopscribble='systemctl --user stop mpdscribble'
+
+# relies on $SHELL being /usr/bin/fish
+if status --is-interactive
+  keychain --eval --quiet --noask -Q id_rsa | source
+end
+
 if which exa ^ /dev/null > /dev/null
   alias la='exa -laF'
   alias ll='exa -lF'
