f8afa11a1fbe88d220ec510601d2ba141f61faae
Peter Rice <ricepv@dukes.jmu.edu>
2017-08-27 22:47:10 -0400

    use exa by default

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,6 @@
 alias startscribble='systemctl --user start mpdscribble'
 alias stopscribble='systemctl --user stop mpdscribble'
+if which exa ^ /dev/null > /dev/null
+  alias la='exa -laF'
+  alias ll='exa -lF'
+end
