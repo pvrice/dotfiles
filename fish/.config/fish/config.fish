c591443d2ab3e067ccb18c4efa056b023e9d5f84
Peter Rice <ricepv@dukes.jmu.edu>
2017-08-27 23:08:27 -0400

    fish syntax

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,10 +3,10 @@ alias stopscribble='systemctl --user stop mpdscribble'
 
 # relies on $SHELL being /usr/bin/fish
 if status --is-interactive
-  keychain --eval --quiet --noask -Q id_rsa | source
+    keychain --eval --quiet --noask -Q id_rsa | source
 end
 
-if which exa ^ /dev/null > /dev/null
-  alias la='exa -laF'
-  alias ll='exa -lF'
+if which exa ^/dev/null >/dev/null
+    alias la='exa -laF'
+    alias ll='exa -lF'
 end
