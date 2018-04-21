36055e122a9eb496aae9863329c75cd6e366df4a
Peter Rice <peter@peterrice.xyz>
2018-04-21 18:26:08 -0400

    add some convenience aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,14 @@
 alias startscribble='systemctl --user start mpdscribble'
 alias stopscribble='systemctl --user stop mpdscribble'
+alias fzf='fzf --bind=ctrl-j:accept,ctrl-k:kill-line'
+alias fzf-tmux='fzf-tmux --bind=ctrl-j:accept,ctrl-k:kill-line'
+alias transmission-remote 'transmission-remote -n $USER:(pass show transmission | head -1)'
 
-# relies on $SHELL being /usr/bin/fish
 if status --is-interactive
-    keychain --eval --quiet --noask -Q id_rsa | source
+    env SHELL=/usr/bin/fish keychain --eval --quiet --noask -Q id_rsa | source
 end
 
-if which exa ^/dev/null >/dev/null
+if which exa > /dev/null 2>&1
     alias la='exa -laF'
     alias ll='exa -lF'
 end
