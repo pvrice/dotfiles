1c4bf7ebdcfd9376e01a308ae6cf9397d2ad189d
Peter Rice <peter@peterrice.xyz>
2018-08-05 17:32:30 -0400

    mutt password support

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -2,6 +2,7 @@ alias fzf 'fzf --bind=ctrl-j:accept,ctrl-k:kill-line'
 alias fzf-tmux 'fzf-tmux --bind=ctrl-j:accept,ctrl-k:kill-line'
 alias pass 'env EDITOR=nvim_nowrite pass'
 alias transmission-remote 'transmission-remote -n $USER:(pass show transmission | head -1)'
+alias neomutt 'neomutt -e "source mutt_pass|"'
 
 if which exa > /dev/null 2>&1
     alias ls 'exa'
