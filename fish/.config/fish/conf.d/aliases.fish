01b7d271b1bd89a7917bb1edcd5fdb578eb63122
Peter Rice <peter@peterrice.xyz>
2018-08-05 17:44:14 -0400

    don't leak transmission password in proc name

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,7 +1,7 @@
 alias fzf 'fzf --bind=ctrl-j:accept,ctrl-k:kill-line'
 alias fzf-tmux 'fzf-tmux --bind=ctrl-j:accept,ctrl-k:kill-line'
 alias pass 'env EDITOR=nvim_nowrite pass'
-alias transmission-remote 'transmission-remote -n $USER:(pass show transmission | head -1)'
+alias transmission-remote 'env TR_AUTH=$USER:(pass show transmission | head -1) transmission-remote -ne'
 alias neomutt 'neomutt -e "source mutt_pass|"'
 
 if which exa > /dev/null 2>&1
