a6324a6f26aab399ef51fad71e97dee415e7455a
Peter Rice <peter@peterrice.xyz>
2019-04-07 01:47:29 -0400

    test if keychain exists before calling

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,3 @@
-if status --is-interactive
-    env SHELL=/usr/bin/fish keychain --agents gpg,ssh --eval --quiet --noask -Q id_rsa | source
+if status --is-interactive && which keychain > /dev/null 2>&1
+    env SHELL=/usr/bin/fish keychain --agents gpg,ssh --eval --noask --quiet -Q id_rsa | source
 end
