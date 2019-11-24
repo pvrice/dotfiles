c89f8675ebbc051c7857bd7346ab852163fb875d
Peter Rice <peter@peterrice.xyz>
2019-11-24 09:49:14 -0500

    use vi mode in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,8 @@
 if status --is-interactive && which keychain > /dev/null 2>&1
     env SHELL=/usr/bin/fish keychain --agents gpg,ssh --eval --noask --quiet -Q id_rsa | source
 end
+
+set -gx fish_key_bindings fish_hybrid_key_bindings
+set -gx fish_cursor_default     block
+set -gx fish_cursor_insert      line
+set -gx fish_cursor_replace_one underscore
